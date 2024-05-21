49cf1e025739fd08bac5823540979d47a288ba5c
hiro-virtozous <naufaldimasazizan@gmail.com>
2024-05-21 08:07:22 +0700

    update Class Page

---
+++ Class Page/class_script.js
@@ -0,0 +1,97 @@
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper .arrow");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+// const showPopup = document.getElementById("wind");
+// const popupContainer = document.querySelector(".popup-container");
+// const closeBtn = document.getElementById("close-wind");
+
+
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+
+//buat dapetin berapa banyak kartu yang fit di setiap carousel
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// ngasih tau kalo gerakin kanan kirinya disini
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "right" ? firstCardWidth : -firstCardWidth;  
+    });
+});
+
+const dragStart = (e) =>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    // update scroll position
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () =>{
+    // ngestop disini kalo mousenya udah ga di grab
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return;
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 1800);
+
+}
+
+autoPlay();
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+        // console.log("mentok kiri");
+    } 
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+        // console.log("mentok kanan");
+    }
+
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+
+// showPopup.onclick = () => {
+//     popupContainer.classList.add("active");
+// }
+
+// closeBtn.onclick = () => {
+//     popupContainer.classList.remove("active");
+// }
+
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
